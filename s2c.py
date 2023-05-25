ee2a3e4621874426ec50a16539ebf40260ff36a8
flynnoct <me@flynmail.com>
2023-05-25 09:39:01 +0800

    Add Cs

---
+++ s2c.py
@@ -0,0 +1,25 @@
+file_name = "Junks.txt"
+
+f1 = open("S/%s" % file_name, "r")
+f2 = open("C/%s" % file_name, "w")
+
+f2.write("payload:\n")
+for line in f1:
+    if line.startswith("DOMAIN-SUFFIX"):
+        domain = line.split(",")[1]
+        new_line = "  - DOMAIN-SUFFIX," + domain
+        f2.write(new_line)
+    elif line.startswith("DOMAIN-KEYWORD"):
+        domain = line.split(",")[1]
+        new_line = "  - DOMAIN-KEYWORD," + domain
+        f2.write(new_line)
+    elif line.startswith("DOMAIN"):
+        domain = line.split(",")[1]
+        new_line = "  - DOMAIN," + domain
+        f2.write(new_line)
+    elif line.startswith("IP-CIDR"):
+        ip_range = line.split(",")[1]
+        new_line = "  - IP-CIDR," + ip_range + "\n"
+        f2.write(new_line)
+
+    
